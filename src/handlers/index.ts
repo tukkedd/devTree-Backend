30af71a3f48f043f50bf12ab68c0817a62037227
tukkedd <jeamki2501@gmail.com>
2024-12-09 23:31:58 -0400

    routing types

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,9 +1,12 @@
+import type { Request, Response } from "express"
 import User from "../models/User"
 
-export const createAccount = async (req, res) => {
+
+
+export const createAccount = async (req: Request, res: Response) => {
     const user = new User(req.body)
     await user.save()
-
+    
     res.send('Registrado correctamente')
     
 }
