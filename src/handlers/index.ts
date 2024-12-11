59a4a65efafb19b79c8a80e47cf1e37a0eca7e01
tukkedd <jeamki2501@gmail.com>
2024-12-10 23:39:49 -0400

    avoid duplicate users

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -4,9 +4,18 @@ import User from "../models/User"
 
 
 export const createAccount = async (req: Request, res: Response) => {
+
+    const { email } = req.body
+
+    const userExists = await User.findOne({email})
+    if(userExists) {
+        const error = new Error('El usuario ya esta registrado')
+        return res.status(409).json({error : error.message})
+    } 
+
     const user = new User(req.body)
     await user.save()
     
-    res.send('Registrado correctamente')
+    res.status(201).send('Registrado correctamente')
     
 }
