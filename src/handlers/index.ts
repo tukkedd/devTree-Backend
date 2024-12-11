765fcdd4bc1cf1ae957da3753cc5f97af9fe4521
tukkedd <jeamki2501@gmail.com>
2024-12-10 23:59:35 -0400

    hashing password, salts

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,19 +1,22 @@
 import type { Request, Response } from "express"
 import User from "../models/User"
-
-
+import { hashPassword } from "../utils/auth"
 
 export const createAccount = async (req: Request, res: Response) => {
 
-    const { email } = req.body
+    const { email, password } = req.body
 
     const userExists = await User.findOne({email})
+    
     if(userExists) {
-        const error = new Error('El usuario ya esta registrado')
-        return res.status(409).json({error : error.message})
+        const err = new Error('El usuario ya esta registrado')
+        res.status(409).json({error : err.message})
+        return
     } 
 
     const user = new User(req.body)
+    hashPassword(password)
+    
     await user.save()
     
     res.status(201).send('Registrado correctamente')
--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import { createAccount } from './handlers';
+
 const router = Router()
 
 // auth and register
-router.post('/auth/register', createAccount)
+router.post('/auth/register', createAccount )
 
 export default router
