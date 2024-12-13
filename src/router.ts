e67595e9ae0ba69ac6fb42bf5363b8ca3d90448b
tukkedd <jeamki2501@gmail.com>
2024-12-12 23:19:22 -0400

    user validation

--- src/router.ts
+++ src/router.ts
@@ -6,7 +6,20 @@ const router = Router()
 
 // auth and register
 router.post('/auth/register', 
-    body('handle').notEmpty(),
+    body('handle')
+        .notEmpty()
+        .withMessage('el handle no puede ir vacio'),
+    body('name')
+        .notEmpty()
+        .withMessage('el "nombre" no puede ir vacio'),
+    body('email')
+        .isEmail()
+        .withMessage('email no valido'),
+    body('password')
+        .isLength({min: 8})
+        .withMessage('el password es muy corto minimo 8 caracteres'),
+
+        
     createAccount )
 
 export default router
